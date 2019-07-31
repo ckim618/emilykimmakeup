09f0dfa434c1a6c1a07824e611fa60bd66307e44
Chris Kim <ckim@havenagency.com>
2019-07-31 09:09:56 -0700

    fix

--- next.config.js
+++ next.config.js
@@ -1,30 +1,25 @@
 const withSass = require('@zeit/next-sass');
 const client = require('./contentful');
 
-
 module.exports = withSass({
     cssModules: true,
     cssLoaderOptions: {
         localIdentName: '[folder]__[local]___[hash:base64:5]',
     },
     webpack(config, options) {
-        config.module.rules.push(
-            {
-                test: /\.scss$/,
-                use: [
-                    'sass-loader',
-                    {
-                        loader: 'sass-resources-loader', // so that we don't need to import scss dependencies on every .scss
-                        options: {
-                            // Or array of paths
-                            resources: [
-                                './resources/scss/imports.scss',
-                            ],
-                        },
+        config.module.rules.push({
+            test: /\.scss$/,
+            use: [
+                'sass-loader',
+                {
+                    loader: 'sass-resources-loader', // so that we don't need to import scss dependencies on every .scss
+                    options: {
+                        // Or array of paths
+                        resources: ['./resources/scss/imports.scss'],
                     },
-                ],
-            }
-        );
+                },
+            ],
+        });
         return config;
     },
 
@@ -33,6 +28,5 @@ module.exports = withSass({
             '/': { page: '/' },
         };
         return paths;
-    }
+    },
 });
-
