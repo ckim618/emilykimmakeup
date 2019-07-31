335222d74d5d0a46f41d89920ffa8f1abe175c09
Chris Kim <ckim@havenagency.com>
2019-07-31 16:07:11 -0700

    got form to work

--- components/ContactForm/index.js
+++ components/ContactForm/index.js
@@ -1,13 +1,6 @@
+import NetlifyForm from 'react-netlify-form';
 import css from './contactform.scss';
 import cc from 'classcat';
-
-const encode = data => {
-    return Object.keys(data)
-        .map(
-            key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
-        )
-        .join('&');
-};
 
 class ContactForm extends React.Component {
     constructor(props) {
@@ -19,109 +12,184 @@ class ContactForm extends React.Component {
             message: '',
             formSuccess: false,
         };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    /* Here’s the juicy bit for posting the form submission */
-
-    handleSubmit = e => {
-        fetch('/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-            body: encode({ 'form-name': 'contact', ...this.state }),
-        })
-            .then(() => {
-                this.setState({
-                    name: '',
-                    email: '',
-                    phone: '',
-                    message: '',
-                    formSuccess: true,
-                });
-                setTimeout(() => {
-                    this.setState({
-                        formSuccess: false,
-                    });
-                }, 8000);
-            })
-            .catch(error => alert(error));
-
-        e.preventDefault();
+    handleSubmit = () => {
+        this.setState({
+            name: '',
+            email: '',
+            phone: '',
+            message: '',
+            formSuccess: true,
+        });
+        setTimeout(() => {
+            this.setState({
+                formSuccess: false,
+            });
+        }, 8000);
     };
 
-    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
 
     render() {
         const { name, email, phone, message } = this.state;
         const formBtnClasses = cc([
-            css.success,
+            css.message,
             {
                 [css.active]: this.state.formSuccess,
             },
         ]);
 
         return (
-            <form
-                name="contact-form"
-                method="post"
-                data-netlify-="true"
-                netlify-honeypot="bot-field"
-                data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
-                className={css.form}
-            >
-                <input type="hidden" name="form-name" value="contact" />
-                <div className={css.inner}>
-                    <p className={css.inputContainer}>
-                        <input
-                            className={css.input}
-                            type="text"
-                            name="name"
-                            value={name}
-                            required
-                            onChange={this.handleChange}
-                            placeholder="Name"
-                        />
-                    </p>
-                    <p className={css.inputContainer}>
-                        <input
-                            className={css.input}
-                            type="email"
-                            name="email"
-                            value={email}
-                            required
-                            onChange={this.handleChange}
-                            placeholder="Email"
-                        />
-                    </p>
-                    <p className={css.inputContainer}>
-                        <input
-                            className={css.input}
-                            type="tel"
-                            name="phone"
-                            required
-                            value={phone}
-                            onChange={this.handleChange}
-                            placeholder="Phone Number"
-                        />
-                    </p>
-                    <p className={css.inputContainer}>
-                        <textarea
-                            className={css.input}
-                            rows="5"
-                            name="message"
-                            value={message}
-                            onChange={this.handleChange}
-                            placeholder="Message"
-                        />
-                    </p>
-                    <button className={css.btn} type="submit">
-                        Send
-                    </button>
-                    <div className={formBtnClasses}>
-                        Thank you! I'll contact you as soon as possible!
+            <NetlifyForm name="contact-form" onSubmit={this.handleSubmit}>
+                {({ loading, error, success }) => (
+                    <div>
+                        {!loading && !success && (
+                            <div className={css.form}>
+                                <div className={css.inner}>
+                                    <input
+                                        type="hidden"
+                                        name="form-name"
+                                        value="contact-form"
+                                    />
+                                    <p className={css.inputContainer}>
+                                        <input
+                                            className={css.input}
+                                            type="text"
+                                            name="name"
+                                            value={name}
+                                            placeholder="Name"
+                                            onChange={this.handleChange}
+                                            required
+                                        />
+                                    </p>
+                                    <p className={css.inputContainer}>
+                                        <input
+                                            className={css.input}
+                                            type="email"
+                                            name="email"
+                                            value={email}
+                                            required
+                                            onChange={this.handleChange}
+                                            placeholder="Email"
+                                        />
+                                    </p>
+                                    <p className={css.inputContainer}>
+                                        <input
+                                            className={css.input}
+                                            type="tel"
+                                            name="phone"
+                                            required
+                                            value={phone}
+                                            onChange={this.handleChange}
+                                            placeholder="Phone Number"
+                                        />
+                                    </p>
+                                    <p className={css.inputContainer}>
+                                        <textarea
+                                            className={css.input}
+                                            rows="5"
+                                            name="message"
+                                            value={message}
+                                            onChange={this.handleChange}
+                                            placeholder="Message"
+                                        />
+                                    </p>
+                                    <button
+                                        on
+                                        className={css.btn}
+                                        type="submit"
+                                    >
+                                        Submit
+                                    </button>
+                                    {loading && (
+                                        <div className={formBtnClasses}>
+                                            Loading...
+                                        </div>
+                                    )}
+                                    {error && (
+                                        <div className={formBtnClasses}>
+                                            Your information was not sent.
+                                            Please try again later.
+                                        </div>
+                                    )}
+                                    {success && (
+                                        <div className={formBtnClasses}>
+                                            Thank you for contacting us!
+                                        </div>
+                                    )}
+                                </div>
+                            </div>
+                        )}
                     </div>
-                </div>
-            </form>
+                )}
+            </NetlifyForm>
+            // <form
+            //     name="contact-form,"
+            //     method="post"
+            //     data-netlify-="true"
+            //     netlify-honeypot="bot-field"
+            //     data-netlify-honeypot="bot-field"
+            //     onSubmit={this.handleSubmit}
+            //     className={css.form}
+            // >
+            //     <input type="hidden" name="form-name" value="contact" />
+            //     <div className={css.inner}>
+            //         <p className={css.inputContainer}>
+            //             <input
+            //                 className={css.input}
+            //                 type="text"
+            //                 name="name"
+            //                 value={name}
+            //                 required
+            //                 onChange={this.handleChange}
+            //                 placeholder="Name"
+            //             />
+            //         </p>
+            //         <p className={css.inputContainer}>
+            //             <input
+            //                 className={css.input}
+            //                 type="email"
+            //                 name="email"
+            //                 value={email}
+            //                 required
+            //                 onChange={this.handleChange}
+            //                 placeholder="Email"
+            //             />
+            //         </p>
+            //         <p className={css.inputContainer}>
+            //             <input
+            //                 className={css.input}
+            //                 type="tel"
+            //                 name="phone"
+            //                 required
+            //                 value={phone}
+            //                 onChange={this.handleChange}
+            //                 placeholder="Phone Number"
+            //             />
+            //         </p>
+            //         <p className={css.inputContainer}>
+            //             <textarea
+            //                 className={css.input}
+            //                 rows="5"
+            //                 name="message"
+            //                 value={message}
+            //                 onChange={this.handleChange}
+            //                 placeholder="Message"
+            //             />
+            //         </p>
+            //         <button className={css.btn} type="submit">
+            //             Send
+            //         </button>
+            //         <div className={formBtnClasses}>
+            //             Thank you! I'll contact you as soon as possible!
+            //         </div>
+            //     </div>
+            // </form>
         );
     }
 }
