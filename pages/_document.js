7f20ecd57134ea4ab694468179112f28c0b6be80
Chris Kim <ckim@havenagency.com>
2019-07-31 20:26:10 -0700

    add meta tags for share

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,19 @@ export default class MyDocument extends Document {
                         name="viewport"
                         content="width=device-width, initial-scale=1, shrink-to-fit=no"
                     />
+                    <meta
+                        property="og:url"
+                        content="http://www.emilykimmakeup.com"
+                    />
+                    <meta property="og:title" content="Emily Kim Makeup" />
+                    <meta
+                        property="og:description"
+                        content="Professional makeup and hair artist available to book for any event!"
+                    />
+                    <meta
+                        property="og:image"
+                        content="http://images.ctfassets.net/i70jiwebm7c8/6ch244bBPtLgbEms1gKyw8/7126ea91b76fad7535b5a883bee6be1c/hero.jpg"
+                    />
                     <link rel="shortcut icon" href="/static/img/brush.png" />
                     <link
                         href="https://fonts.googleapis.com/css?family=Dosis&display=swap"
