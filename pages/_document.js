02a05ad3314b2d47c0a09e7953af8f2045764e4a
Chris Kim <ckim@havenagency.com>
2019-08-01 09:11:20 -0700

    add polyfill

--- pages/_document.js
+++ pages/_document.js
@@ -36,6 +36,7 @@ export default class MyDocument extends Document {
                 <body>
                     <Main />
                     <NextScript />
+                    <script src="https://cdn.polyfill.io/v2/polyfill.min.js" />
                 </body>
             </html>
         );
